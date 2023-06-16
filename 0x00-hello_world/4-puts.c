11569edc1d30bdf359705eb450fa282d07e1f576
Alpomann <chalice09b@gmail.com>
2023-06-15 18:36:28 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - A program that prints a line with puts funtions
+ * Retrun: 0 (success)
+ */
+init main(void)
+{
+puts(" \"Programming is like building a multilingual puzzle\n");
+return (0);
+}
